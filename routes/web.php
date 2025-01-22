ecb8768508a319bf49768d31cc4a8d1f458a75b4
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-22 11:07:22 -0300

    Foi criada um Controller e uma rota apontando para um método desse Controller.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Olá Mundo!";
 });
+
+
+Route::get('/about', function () {
+    echo "About Us!";
+});
+
+// Criar uma rota diretamente para um método dentro de uma classe
+Route::get('/main', [MainController::class, 'index']);
