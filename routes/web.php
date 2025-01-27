9f9b5acf979d0386d577ff0b342fd17f2559a4ec
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-27 07:42:43 -0300

    Limpando o projeto

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('principal');
-});
 
-
-
-Route::get('/about', function () {
-    echo "About Us!";
-});
-
-// Criar uma rota diretamente para um método dentro de uma classe
-// Adicionando um parâmetro a rota: /main para /main{value}
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
