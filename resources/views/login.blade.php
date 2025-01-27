8aa78cc07fd6937d63f3372cd646e1539114b265
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-27 12:13:47 -0300

    Mostrar erros nos inputs e manter dados digitados

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,60 +1,59 @@
 @extends('layouts.main_layout')
 @section('content')
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-sm-8">
+                <div class="card p-5">
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6 col-sm-8">
-            <div class="card p-5">
+                    <!-- logo -->
+                    <div class="text-center p-3">
+                        <img src="assets/images/logo.png" alt="Notes logo">
+                    </div>
 
-                <!-- logo -->
-                <div class="text-center p-3">
-                    <img src="assets/images/logo.png" alt="Notes logo">
-                </div>
-
-                <!-- form -->
-                <div class="row justify-content-center">
-                    <div class="col-md-10 col-12">
-                        <form action="/loginSubmit" method="post">
-                            {{--
+                    <!-- form -->
+                    <div class="row justify-content-center">
+                        <div class="col-md-10 col-12">
+                            <form action="/loginSubmit" method="post">
+                                {{--
                                 Cross-site Request Forgery (CSRF) é um tipo de ataque de websites maliciosos.
                                 Um ataque CSRF às vezes é chamado de ataque de um clique ou transporte de sessão.
                                 Esse tipo de ataque envia solicitações desautorizadas de um usuário no qual o
                                 website confia.
                             --}}
-                            @csrf
-                            <div class="mb-3">
-                                <label for="text_username" class="form-label">Username</label>
-                                <input type="text" class="form-control bg-dark text-info" name="text_username">
-                            </div>
-                            <div class="mb-3">
-                                <label for="text_password" class="form-label">Password</label>
-                                <input type="password" class="form-control bg-dark text-info" name="text_password">
-                            </div>
-                            <div class="mb-3">
-                                <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
-                            </div>
-                        </form>
-                        {{-- errors --}}
-                        @if ($errors->any())
-                            <div class="alert alert-danger mt-3">
-                                <ul class="m-0">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="text_username" class="form-label">Username</label>
+                                    <input type="text" class="form-control bg-dark text-info" name="text_username"
+                                        value="{{ old('text_username') }}">
+                                    {{-- Show errors --}}
+                                    @error('text_username')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="text_password" class="form-label">Password</label>
+                                    <input type="password" class="form-control bg-dark text-info" name="text_password"
+                                        value="{{ old('text_password') }}">
+                                    {{-- Show errors --}}
+                                    @error('text_password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
-                </div>
 
-                <!-- copy -->
-                <div class="text-center text-secondary mt-3">
-                    <small>&copy; <?= date('Y') ?> Notes</small>
-                </div>
+                    <!-- copy -->
+                    <div class="text-center text-secondary mt-3">
+                        <small>&copy; <?= date('Y') ?> Notes</small>
+                    </div>
 
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
