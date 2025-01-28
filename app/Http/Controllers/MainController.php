7cdbde91a84a87b7c43dbf6883d3e97fb1635750
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-28 09:41:22 -0300

    Introdução ao Middleware

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    
+    public function index()
+    {
+        echo "Estou no APP";
+    }
+
+    public function newNote()
+    {
+        echo "Criando nova Nota";
+    }
 }
