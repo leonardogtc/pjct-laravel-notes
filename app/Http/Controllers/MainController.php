6e71cf3f75b2a004f2a54c9cdf151913c50ca9d4
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-28 23:34:09 -0300

    Carregando as notas do usuário logado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+        // Load user's notes (Pega as notas do usuário)
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        echo "<pre>";
+        print_r($user);
+        print_r($notes);
+
+        die();
+
+        // Mostrar a view 'home'
         return view('home');
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    // Criar relação de chave extrangeira entre User -> Note
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
 }
