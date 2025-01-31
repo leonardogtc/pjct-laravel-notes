b9e84dda59da51fc56efbcda7a24ff48ecd0b01d
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-31 08:09:31 -0300

    Criando Notas e Validação do Formulário

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Http\Request;
@@ -25,7 +26,35 @@ class MainController extends Controller
 
     public function newNoteSubmit(Request $request)
     {
-        echo "Estou criando uma nova nota";
+        // Validar requisição
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required' => 'O título da nota é obrigatório',
+                'text_title.min' => 'O título deve ter no mínimo :min caracteres',
+                'text_title.max' => 'O título pode ter no máximo :max caracteres',
+
+                'text_note.required' => 'A descrição da nota é obrigatória',
+                'text_note.min' => 'A nota deve ter no mínimo :min caracteres',
+                'text_note.max' => 'A nota pode ter no máximo :max caracteres'
+
+            ]
+        );
+        // Capturar o ID so usuário
+        $id = session('user.id');
+
+        // Criar uma nova nota
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // Redirecionar para home
+        return redirect()->route('home');
     }
 
     public function editNote($id)
@@ -39,6 +68,4 @@ class MainController extends Controller
         $id = Operations::decryptID($id);
         echo "Deletando nota de ID = $id";
     }
-
-
 }
--- resources/views/new_note.blade.php
+++ resources/views/new_note.blade.php
@@ -25,17 +25,27 @@
                         <div class="col">
                             <div class="mb-3">
                                 <label class="form-label">Note Title</label>
-                                <input type="text" class="form-control bg-primary text-white" name="text_title">
+                                <input type="text" class="form-control bg-primary text-white" name="text_title"
+                                    value="{{ old('text_title') }}">
+                                {{-- Show error --}}
+                                @error('text_title')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Note Text</label>
-                                <textarea class="form-control bg-primary text-white" name="text_note" rows="5"></textarea>
+                                <textarea class="form-control bg-primary text-white" name="text_note" rows="5">{{ old('text_note') }}</textarea>
+                                {{-- show error --}}
+                                @error('text_note')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="col text-end">
-                            <a href="{{ route('home') }}" class="btn btn-primary px-5"><i class="fa-solid fa-ban me-2"></i>Cancel</a>
+                            <a href="{{ route('home') }}" class="btn btn-primary px-5"><i
+                                    class="fa-solid fa-ban me-2"></i>Cancel</a>
                             {{-- NOTA: Quando não existir no formulário o @csrf será mostrado um erro "419 Page Expired" --}}
                             <button type="submit" class="btn btn-secondary px-5"><i
                                     class="fa-regular fa-circle-check me-2"></i>Save</button>
